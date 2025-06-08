86d02a9cb1ce82f5d8db16dcd9d1a6dc7b188668
Enoch208 <ifeoluwaidowu2008@gmail.com>
2025-06-08 09:11:51 -0700

    MAde some fixes

--- travidox_frontend/build-vercel.sh
+++ travidox_frontend/build-vercel.sh
@@ -3,7 +3,7 @@ set -e
 
 # Optional: run any import fix or prep
 echo "Fixing imports..."
-npm run fix-imports # Or whatever you call that step
+# Or whatever you call that step
 
 echo "Running build..."
 npm run build
